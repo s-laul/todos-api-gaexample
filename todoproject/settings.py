d219a1b21ae7622192ef721fb536f27ec35816d7
slaul <sameer.laul@uconn.edu>
2023-04-11 19:22:39 -0400

    settings.py updated

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "todos.apps.TodosConfig",
-    "rest_framework"
+    "rest_framework",
     "corsheaders"
 ]
 
